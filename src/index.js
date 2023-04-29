53e7d3f91e449b49c666f22a12a5c49f94b91cc2
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-04-28 17:14:59 -0700

    Break 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './index.less'
 import './components/article/article.js'
 import './components/menu/menu.js'
+
+function articleMaker(title){
+    const article = document.createElement('div');
+    article.classList.add(article);
+    const name = document.createElement(h2);
+    name.textContent.add(title);
+    const paragraph = document.createElement(p);
+    paragraph.classList.add(date);
+    paragraph.textContent.add('date of the article');
+    const expand = document.createElement(span);
+    expand.classList(expandButton);
+    expand.addEventListener('click', (event) => {
+        expand('the button cliked expands')
+    })
+
+    return articleMaker; 
+}
